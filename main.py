16ca96a3f9203e7f924b2e1119bce3cd8715cae7
masimkhanneila <neyl20052008@gmail.com>
2026-08-11 14:44:04 +0500

    renamed and added \n command

---
+++ main.py
@@ -0,0 +1,68 @@
+import sys
+
+
+def read_make_dict(file):
+    with open(file, "r") as f:
+        f.readline()
+        f.readline()
+        value = [ 
+            f.readline().rstrip("\n")
+            for i in range(8) 
+            ]
+        d = {
+            " " : value
+        }
+ 
+        for k in range(33, 127):
+            value = [ 
+                f.readline().rstrip("\n")
+                for i in range(8) 
+                ]
+            d[chr(k)] = value
+            f.readline()
+        k += 1
+
+    return d
+standard= read_make_dict("standard.txt")
+shadow= read_make_dict("shadow.txt")
+thinkertoy= read_make_dict("thinkertoy.txt")
+"""
+for key, value in thinkertoy.items():
+    print(f"Character: {repr(key)}")
+    for line in value:
+        print(line)
+    print("-" * 20)
+"""
+
+def print_ascii_art():
+    if len(to_print) >= 14:
+        print("Too many characters. Please enter less than 14 characters.")
+    
+    for i in range(8):
+        line = ""
+        for char in to_print:
+            if char in standard:
+                line += standard[char][i] + " "
+        print(line)
+        
+to_print = sys.argv[1]
+if "\\n" in to_print:
+    words = to_print.split("\\n")
+    for word in words:
+        to_print = word
+        print_ascii_art()
+        print("\n")
+else:
+    print_ascii_art()
+
+
+    
+
+
+
+
+
+
+
+
+
