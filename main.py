3278517ebf3189739d06444b3ddb83f1f9ed89bd
masimkhanneila <neyl20052008@gmail.com>
2026-08-11 19:55:27 +0500

    changed input

--- main.py
+++ main.py
@@ -23,37 +23,57 @@ def read_make_dict(file):
         k += 1
 
     return d
-standard= read_make_dict("standard.txt")
-shadow= read_make_dict("shadow.txt")
-thinkertoy= read_make_dict("thinkertoy.txt")
-"""
-for key, value in thinkertoy.items():
-    print(f"Character: {repr(key)}")
-    for line in value:
-        print(line)
-    print("-" * 20)
-"""
+standarD= read_make_dict("standard.txt")
+shadoW= read_make_dict("shadow.txt")
+thinkertoY= read_make_dict("thinkertoy.txt")
 
-def print_ascii_art():
+def print_ascii_art(to_print,file):
+    result=""
     if len(to_print) >= 14:
         print("Too many characters. Please enter less than 14 characters.")
+        to_print = "Too long"
     
+
     for i in range(8):
         line = ""
         for char in to_print:
-            if char in standard:
-                line += standard[char][i] + " "
-        print(line)
-        
+            if char in file:
+                line += file[char][i] + " "
+            result += line + "\n"
+    return result
+
+def which_font(f= "standard"):
+    if f.lower() == "shadow" :
+        return shadoW
+    elif f.lower() == "thinkertoy":
+        return thinkertoY
+    else :
+        return standarD
+
+
 to_print = sys.argv[1]
-if "\\n" in to_print:
-    words = to_print.split("\\n")
-    for word in words:
-        to_print = word
-        print_ascii_art()
-        print("\n")
-else:
-    print_ascii_art()
+font = sys.argv[-1]
+for_output = ""
+if sys.argv[1].startswith("--output="):
+    for_output = sys.argv[1].split("=")[1]
+    to_print = sys.argv[2]
+
+file = which_font(font)
+
+if for_output=="":
+    if "\\n" in to_print:
+        words = to_print.split("\\n")
+        for word in words:
+            to_print = word
+            print(print_ascii_art(to_print,file))
+            print("\n")
+    else:
+        print(print_ascii_art(to_print,file))
+else :
+    with open(for_output,"w"):
+        for_output.write(print_ascii_art(to_print,file))
+    print("File " + for_output + " was created")
+
 
 
     
