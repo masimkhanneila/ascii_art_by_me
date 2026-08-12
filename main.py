e63283b31abc44e700a64f74b110f9293ba1f163
masimkhanneila <neyl20052008@gmail.com>
2026-08-12 14:35:35 +0500

    added argparse parser for the all arguments

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import sys
+import argparse
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--output", help="Output file")
+parser.add_argument("--color", choices=["black", "red", "green", "yellow", "blue", "magenta", "cyan", "white", "orange", "pink", "purple", "light_blue"], help="Color of the ASCII art")
+parser.add_argument("letters", nargs="?", help="Letters to convert to color")
+parser.add_argument("--align", choices=["left", "center", "right"], default="left", help="Alignment of the ASCII art")
+parser.add_argument("--reverse", action="store_true", help="Reverse the order of the ASCII art")
+parser.add_argument("text", help="Text to convert to ASCII art")
+parser.add_argument("font",nargs="?",choices=["standard", "shadow", "thinkertoy"],default="standard")
+args = parser.parse_args()
+
+colors = {
+    "black": "\033[30m",
+    "red": "\033[31m",
+    "green": "\033[32m",
+    "yellow": "\033[33m",
+    "blue": "\033[34m",
+    "magenta": "\033[35m",
+    "cyan": "\033[36m",
+    "white": "\033[37m",
+    "orange": "\033[38;5;208m",
+    "pink": "\033[38;5;13m",
+    "purple": "\033[38;5;93m",
+    "light_blue": "\033[38;5;117m",
+    "reset": "\033[0m"
+}
 
 def read_make_dict(file):
     with open(file, "r") as f:
@@ -51,16 +78,19 @@ def which_font(f= "standard"):
         return standarD
 
 
-to_print = sys.argv[1]
-font = sys.argv[-1]
-for_output = ""
-if sys.argv[1].startswith("--output="):
-    for_output = sys.argv[1].split("=")[1]
-    to_print = sys.argv[2]
+to_print = args.text
+font = args.font
+for_output = args.output
+color = args.color
+letters = args.letters
+align = args.align
+reverse = args.reverse
+
+print(for_output, color, letters, align, reverse, to_print, font)
 
 file = which_font(font)
 
-if for_output=="":
+if for_output==None:
     if "\\n" in to_print:
         words = to_print.split("\\n")
         for word in words:
